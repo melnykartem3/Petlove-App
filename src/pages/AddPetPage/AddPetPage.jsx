b2cb605554ad59dc70942c19ce4292ab98610c4a
Artem Melnik <melnikartem@vesele-gimnaz.com>
2024-12-17 23:33:18 +0200

    fix imports in addPetPage

--- src/pages/AddPetPage/AddPetPage.jsx
+++ src/pages/AddPetPage/AddPetPage.jsx
@@ -1,9 +1,9 @@
 import addPetMobile from '../../images/addPet/addPetMobile.jpg';
-import addPetMobile_2x from '../../images/addPet/addPetMobile.jpg@2x';
+import addPetMobile_2x from '../../images/addPet/addPetMobile@2x.jpg';
 import addPetTablet from '../../images/addPet/addPetTablet.jpg';
-import addPetTablet_2x from '../../images/addPet/addPetTablet.jpg@2x';
+import addPetTablet_2x from '../../images/addPet/addPetTablet@2x.jpg';
 import addPetDesktop from '../../images/addPet/addPetDesktop.jpg';
-import addPetDesktop_2x from '../../images/addPet/addPetDesktop.jpg@2x';
+import addPetDesktop_2x from '../../images/addPet/addPetDesktop@2x.jpg';
 import css from './AddPetPage.module.css';
 
 export default function AddPetPage() {
